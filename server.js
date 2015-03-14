8b2c4d3db77eeca21d40acd78a8e4135482ce8cd
Omar Sabry <ohsabry@gmail.com>
2015-03-14 16:29:01 -0700

    enable cors

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
+
+
 router.get('/', function(req, res) {
     res.json({ message: 'welcome to the shelf life api' });
 });
